b2ea4ed18ea5402af5637c3c4ec878f819b4e433
AndreZara1999 <andrea.zarantonello99@gmail.com>
2021-03-27 08:50:43 +0100

    Add simple fetch request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App({ user }) {
+  const [data, setData] = useState(null);
+
+  useEffect(_ => {
+    fetch(`https://api.github.com/users/${user}`)
+      .then((response) => response.json())
+      .then(setData)
+      .catch(error => console.log(`Error! Error! Error! -- - ${error}`));
+  }, []);
+
+  if(data) {
+    return <div>{ JSON.stringify(data) }</div>
+  }
+  else {
+    return <div>No user available</div>
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App user="AndreZara1999" />
   </React.StrictMode>,
   document.getElementById('root')
 );
