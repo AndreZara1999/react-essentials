2468daa03156d283870c9ab5b8d95b9dd97cb980
AndreZara1999 <andrea.zarantonello99@gmail.com>
2021-03-27 08:15:19 +0100

    Add a useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 
 function ShowMe() {
   return <h1>I'm <span style={{ color : 'green' }}>show</span> to you becouse you <span style={{color : 'green'}}>are authorized!</span></h1>
@@ -10,30 +10,15 @@ function HideMe() {
 }
 
 function App() {
-  const [authorized, setAuthorized] = useState(false);
-  
-  /**
-   * Only one param -- - Work with the first redenring and every time 
-   * that the state chages
-   * 
-   * Two params that the second is empty array -- - Work for only the
-   * render
-   * 
-   * Two params that second is an Array of value (useState) -- - Work
-   * for render and when the state chenge, but show only state that I 
-   * passed in the array  
-   */
-  useEffect(_ => {
-    console.log(`State: ${authorized}`);
-  }, [authorized])
+  const [authorized, handleClick] = useReducer(
+    (checked) => !checked, 
+    false
+  );
 
   return (
     <>
       { authorized ? <ShowMe /> : <HideMe /> }
-
-      { authorized 
-        ? <button onClick={() => setAuthorized(!authorized)}>Revocation authorization</button> 
-        : <button onClick={() => setAuthorized(!authorized)}>Allow authorization</button> }
+      <button onClick={handleClick}>Handle authorizations</button> 
     </>
   );
 }
