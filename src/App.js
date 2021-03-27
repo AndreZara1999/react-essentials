c23c6a5694309c6fdcd1d81eaf33648785c1c8d2
AndreZara1999 <andrea.zarantonello99@gmail.com>
2021-03-27 07:53:38 +0100

    Add state and manage with hook useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useState } from 'react';
 
 function ShowMe() {
   return <h1>I'm <span style={{ color : 'green' }}>show</span> to you becouse you <span style={{color : 'green'}}>are authorized!</span></h1>
@@ -8,10 +9,15 @@ function HideMe() {
   return <h1>I'm <span style={{color : 'red'}}>hide</span> to you becouse you <span style={{color : 'red'}}>aren't authorized!</span></h1>
 }
 
-function App(props) {
+function App() {
+  const [authorized, setAuthorized] = useState(false);
   return (
     <>
-      { props.authorized ? <ShowMe /> : <HideMe /> }
+      { authorized ? <ShowMe /> : <HideMe /> }
+
+      { authorized 
+        ? <button onClick={() => setAuthorized(!authorized)}>Revocation authorization</button> 
+        : <button onClick={() => setAuthorized(!authorized)}>Allow authorization</button> }
     </>
   );
 }
